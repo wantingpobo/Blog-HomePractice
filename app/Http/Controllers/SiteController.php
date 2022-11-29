1bd82dcc28694e045f50ee64ec8c6a661c86ab03
wanting <wantingpobo@gamil.com>
2022-11-29 09:43:49 +0800

    取得請求資料

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class SiteController extends Controller
 {
-    public function test()
+    public function test(Request $request)
     {
+        //取得用戶所有資料
+        dd($request->all());
+
+        //確認欄位是否有填寫>顯示ture或false
+        // dd($request->filled('title'));
+
+        //直接用欄位來取值
+        //dd($request->x);
+
+        //取得除了age以外的資料
+        //dd($request->except('age'));
+
+        //只取某些值,可用陣列擺多值
+        //dd($request->only(['x','y']));
+
+        // -----------------------------------
+
         //第一種:把值傳入視圖的$name和$age變數
         //return view('example.1120demo')->with(['name' => 'tom', 'age' => '18']);
 
